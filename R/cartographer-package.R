cbf466bc1daf1bd628ab2d3aa29c7af4312bb6d6
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-12-22 22:57:17 -0500

    Document package

--- R/cartographer-package.R
+++ R/cartographer-package.R
@@ -1,5 +1,11 @@
 #' Interactive Maps with d3-carto-map
 #'
+#' This package provides an R interface to
+#' \href{https://github.com/emeeks/d3-carto-map}{d3-carto-map} (created by
+#' \href{https://twitter.com/elijah_meeks}{Elijah Meeks}) and the
+#' \href{http://d3js.org/}{d3.js} visualization library (created by Mike
+#' Bostock). It lets you create interactive maps at the R console and in R
+#' documents.
 #' @name cartographer
 NULL
 
